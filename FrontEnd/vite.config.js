51604fe4c66137a7bcda3cc8a712118c28621142
Jafarabat2025 <dzhafar.idrisov@bk.ru>
2025-10-18 18:39:20 +0300

    fix cert

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { getCert } from "./cert/createCert.js"; 
 
 export default defineConfig(async () => {
-  const { key, cert } = await getCert(); 
 
   return {
     plugins: [react()],
     server: {
       https: {
-        key,
-        cert,
         port: 3000
       },
     },
